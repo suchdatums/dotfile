bc2af891b1ec94b6c85666acbeadfe73a38fd52d
micah <micahjcfull@gmail.com>
2022-01-16 12:45:58 -0800

    gg

--- get_repo.sh
+++ get_repo.sh
@@ -3,6 +3,15 @@
 echo "what repo???"
 read repo
 url="https://github.com/suchdatums/$repo"
-git clone --depth=1 $url
+git clone --depth=1 --recurse-submodules $url
 rm -rf ./$repo/.git
-rm .gitignore
+rm ./$repo/.gitignore
+
+echo ">>> DO YOU WANT TO INSTALL IN THE HOME FOLDER?"
+echo "type: yes"
+read inp
+if [-ne inp "yes"] then
+exit
+fi
+mv -f ./$repo/* .
+rm -r ./$repo/
